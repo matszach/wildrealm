bdc05c0772db71c72d494665e01be5f13360b991
matszach <lkaszubowski@gmail.com>
2019-10-15 23:04:50 +0200

    view button placing in for loop reworked

--- src/display_module/views/load_game_view.py
+++ src/display_module/views/load_game_view.py
@@ -9,12 +9,32 @@ class LoadGameView(View):
         from src.game_core_module.game import Game
 
         # save state buttons
-        for i in range(1, 5):
-            load_button = Button(12, 3 + i * 3, 11, 2, f'Save state {i}',
-                                 lambda: Game.load_game(i))
-            if Game.load_file_exists(i):
-                load_button.activate()
-            self.nodes.append(load_button)
+        load_button_1 = Button(12, 3, 11, 2, f'Save state 1',
+                               lambda: Game.load_game(1))
+        if Game.load_file_exists(1):
+            load_button_1.activate()
+        self.nodes.append(load_button_1)
+        load_button_2 = Button(12, 6, 11, 2, f'Save state 2',
+                               lambda: Game.load_game(2))
+        if Game.load_file_exists(2):
+            load_button_2.activate()
+        self.nodes.append(load_button_2)
+        load_button_3 = Button(12, 9, 11, 2, f'Save state 3',
+                               lambda: Game.load_game(3))
+        if Game.load_file_exists(3):
+            load_button_3.activate()
+        self.nodes.append(load_button_3)
+        load_button_4 = Button(12, 12, 11, 2, f'Save state 4',
+                               lambda: Game.load_game(4))
+        if Game.load_file_exists(4):
+            load_button_4.activate()
+        self.nodes.append(load_button_4)
+        load_button_5 = Button(12, 15, 11, 2, f'Save state 5',
+                               lambda: Game.load_game(5))
+        if Game.load_file_exists(5):
+            load_button_5.activate()
+        self.nodes.append(load_button_5)
+
 
         # return button
         return_button = Button(13, 18, 9, 2, 'Return',
--- src/display_module/views/new_game_view.py
+++ src/display_module/views/new_game_view.py
@@ -9,18 +9,26 @@ class NewGameView(View):
         from src.game_core_module.game import Game
 
         # new game buttons
-        small_world_button = Button(12, 9, 11, 2, f'Small [256x256]',
-                                    lambda: Game.start_new_game(256))
+        tiny_world_button = Button(12, 3, 11, 2, f'Tiny [256x256]',
+                                   lambda: Game.start_new_game(256))
+        tiny_world_button.activate()
+        self.nodes.append(tiny_world_button)
+        small_world_button = Button(12, 6, 11, 2, f'Small [512x512]',
+                                    lambda: Game.start_new_game(512))
         small_world_button.activate()
         self.nodes.append(small_world_button)
-        medium_world_button = Button(12, 12, 11, 2, f'Medium [512x512]',
-                                     lambda: Game.start_new_game(512))
+        medium_world_button = Button(12, 9, 11, 2, f'Medium [1024x1024]',
+                                     lambda: Game.start_new_game(1024))
         medium_world_button.activate()
         self.nodes.append(medium_world_button)
-        large_world_button = Button(12, 15, 11, 2, f'Large [1024x1024]',
-                                    lambda: Game.start_new_game(1024))
+        large_world_button = Button(12, 12, 11, 2, f'Large [2048x2048]',
+                                    lambda: Game.start_new_game(2048))
         large_world_button.activate()
         self.nodes.append(large_world_button)
+        huge_world_button = Button(12, 15, 11, 2, f'Huge [4096x4096]',
+                                   lambda: Game.start_new_game(4096))
+        huge_world_button.activate()
+        self.nodes.append(huge_world_button)
 
         # return button
         return_button = Button(13, 18, 9, 2, 'Return',
