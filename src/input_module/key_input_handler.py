09654645743bdc316b915bf66e24607ebd90a934
matszach <lkaszubowski@gmail.com>
2019-10-16 20:02:41 +0200

    very basic key input support

--- src/input_module/key_input_handler.py
+++ src/input_module/key_input_handler.py
@@ -1,5 +1,6 @@
 from src.game_core_module.app_states import AppStates
 from src.game_core_module.game_state.game_state import GameState
+import pygame
 
 
 class KeyInputHandler:
@@ -7,5 +8,17 @@ class KeyInputHandler:
     """
     handles user input based on app state
     """
-    def handle(self, app_state: int, game_state: GameState):
-        pass  # todo
+    @staticmethod
+    def handle(app_state: int, game_state: GameState):
+
+        if app_state == AppStates.IN_GAME_PLAY:
+            keys = pygame.key.get_pressed()
+            player = game_state.player
+            if keys[pygame.K_w]:
+                player.y -= 1
+            elif keys[pygame.K_s]:
+                player.y += 1
+            elif keys[pygame.K_a]:
+                player.x -= 1
+            elif keys[pygame.K_d]:
+                player.x += 1
